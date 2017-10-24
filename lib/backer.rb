269f961e57eba2c224fe64fd9059dd676532d3f2
Trever Andersen <trever.r.andersen@gmail.com>
2017-10-24 03:45:30 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+require 'pry'
+class Backer
+  attr_accessor :name, :title, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(title)
+    @backed_projects << title
+  end
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(name)
+    @backers << name
+  end
+
+end
